233ca4cc2c512d9321b0ac36e779c6da34e52d7d
egarcia <egarcia@gtssl.com>
2024-01-02 23:52:53 +0100

    3.1 Consejos sobre migraciones y ajustando la migracion de usuarios
    	modified:   database/migrations/2014_10_12_000000_create_users_table.php

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -6,25 +6,25 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-    /**
-     * Run the migrations.
-     */
     public function up(): void
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id('user_id');
+
+            $table->boolean('is_admin')->default(false);
+            $table->boolean('is_active')->default(false);
+
             $table->string('name');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+
             $table->rememberToken();
             $table->timestamps();
+            $table->softDeletes();
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
     public function down(): void
     {
         Schema::dropIfExists('users');
