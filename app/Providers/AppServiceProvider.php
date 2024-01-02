e2c7a51ce707e74b570bc03a769d038fcc2020ef
egarcia <egarcia@gtssl.com>
2024-01-02 23:43:53 +0100

    2.2 Registrando las macros de la clase Blueprint
    	modified:   app/Providers/AppServiceProvider.php

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -11,7 +11,18 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        //
+        // use blueprint macros on App\Macros\Blueprint
+        // loop through all macros in the directory
+        foreach (glob(app_path('Macros/Blueprint/*.php')) as $filename) {
+            // get the filename
+            $filename = basename($filename, '.php');
+
+            // get the class name
+            $class = 'App\\Macros\\Blueprint\\'.$filename;
+
+            // register the macro
+            $this->app->call($class);
+        }
     }
 
     /**
