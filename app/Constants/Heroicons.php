5aa525f0800d331a732027f140e21c096ae5daa8
egarcia <egarcia@gtssl.com>
2024-01-05 00:45:39 +0100

    7.1 Clase de constantes para representar iconos del Frontend
    	new file:   app/Constants/Heroicons.php

---
+++ app/Constants/Heroicons.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Constants;
+
+final class Heroicons
+{
+    public const HOME = 'HomeIcon';
+
+    public const USERS = 'UsersIcon';
+
+    public const USER = 'UserIcon';
+
+    public const PLUS = 'PlusCircleIcon';
+
+    public const DOWNLOAD = 'ArrowDownOnSquareIcon';
+
+    public const CALENDAR = 'CalendarIcon';
+
+    public const TAG = 'TagIcon';
+
+    public const QUESTION_MARK_CIRCLE = 'QuestionMarkCircleIcon';
+
+    public const BOOK_OPEN = 'BookOpenIcon';
+
+    public const LOGOUT = 'ArrowRightOnRectangleIcon';
+}
