786e1565ed48c2badbf3a2fabfea8a76b68e1fe3
egarcia <egarcia@gtssl.com>
2024-01-04 02:25:13 +0100

    5.3 Extender las rutas de Laravel a través del trait Macroable
    	modified:   app/Providers/RouteServiceProvider.php

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -24,9 +24,13 @@ class RouteServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
+        parent::boot();
+
         RateLimiter::for('api', function (Request $request) {
             return Limit::perMinute(60)->by($request->user()?->id ?: $request->ip());
         });
+
+        $this->registerJsonGroupMacro();
 
         $this->routes(function () {
             Route::middleware('api')
@@ -41,6 +45,36 @@ class RouteServiceProvider extends ServiceProvider
                 ->prefix('backoffice')
                 ->as('backoffice.')
                 ->group(base_path('routes/backoffice.php'));
+        });
+    }
+
+    private function registerJsonGroupMacro(): void
+    {
+        Route::macro('jsonGroup', function (string $prefix, string $controller, array $methods) {
+            Route::prefix($prefix)->name($prefix.'.')->group(function () use ($controller, $methods) {
+                if (in_array('index', $methods)) {
+                    Route::get('/', [$controller, 'index'])->name('index');
+                }
+                if (in_array('json', $methods)) {
+                    Route::get('/json', [$controller, 'json'])->name('json');
+                }
+                if (in_array('export', $methods)) {
+                    Route::post('/generate-export-url', [$controller, 'generateExportUrl'])->name('generate_export_url');
+                    Route::get('/export', [$controller, 'export'])->name('export');
+                }
+                if (in_array('show', $methods)) {
+                    Route::get('/{id}', [$controller, 'show'])->name('show');
+                }
+                if (in_array('store', $methods)) {
+                    Route::post('/', [$controller, 'store'])->name('store');
+                }
+                if (in_array('update', $methods)) {
+                    Route::put('/{id}', [$controller, 'update'])->name('update');
+                }
+                if (in_array('destroy', $methods)) {
+                    Route::delete('/{id}', [$controller, 'destroy'])->name('destroy');
+                }
+            });
         });
     }
 }
