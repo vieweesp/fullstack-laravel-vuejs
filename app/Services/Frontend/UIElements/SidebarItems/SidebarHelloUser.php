7aef7abc9e6a5fb04382e6f429cf88643d1e5f32
egarcia <egarcia@gtssl.com>
2024-01-05 02:53:14 +0100

    9.3 Definir el componente SidebarHelloUser con Laravel
    	new file:   app/Services/Frontend/UIElements/SidebarItems/SidebarHelloUser.php

---
+++ app/Services/Frontend/UIElements/SidebarItems/SidebarHelloUser.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Services\Frontend\UIElements\SidebarItems;
+
+use App\Services\Frontend\UIElements\SidebarItems\Contracts\SidebarItem;
+
+class SidebarHelloUser implements SidebarItem
+{
+    const COMPONENT = 'AppSidebarHelloUserItem';
+
+    public function toArray(): array
+    {
+        return [
+            'component' => self::COMPONENT,
+            'text' => __('Hola, :name', ['name' => auth()->user()->name]),
+        ];
+    }
+}
