a76684b39f5d883ab1f448a87841b103e9713039
egarcia <egarcia@gtssl.com>
2024-01-08 23:54:58 +0100

    12.4 Componente Vue SidebarSeparator
    	new file:   resources/js/components/ui/sidebars/items/AppSidebarSeparatorItem.vue

---
+++ resources/js/components/ui/sidebars/items/AppSidebarSeparatorItem.vue
@@ -0,0 +1,11 @@
+<script>
+export default {
+    name: 'AppSidebarSeparatorItem',
+}
+</script>
+
+<template>
+    <li class="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
+        <span class="flex-1 h-px bg-gray-300 dark:bg-gray-700" aria-hidden="true"></span>
+    </li>
+</template>
