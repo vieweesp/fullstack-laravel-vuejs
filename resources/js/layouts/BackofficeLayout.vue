44cd304f179de5278919cee6510db177d01a6ff8
egarcia <egarcia@gtssl.com>
2024-01-09 00:59:25 +0100

    13.1 Crear el BackofficeLayout con Vue
    	new file:   resources/js/layouts/BackofficeLayout.vue

---
+++ resources/js/layouts/BackofficeLayout.vue
@@ -0,0 +1,48 @@
+<script>
+export default {
+    name: 'BackOfficeLayout',
+}
+</script>
+
+<script setup>
+import { AppSidebar } from '@/components/ui/sidebars'
+
+defineProps({
+    title: {
+        type: String,
+        required: true,
+    },
+    user: {
+        type: Object,
+        required: true,
+    },
+    sidebarNavItems: {
+        type: Array,
+        required: true,
+    },
+})
+</script>
+
+<template>
+    <AppSidebar
+        :title="title"
+        :user="user"
+        :sidebarNavItems="sidebarNavItems"
+    />
+
+    <div class="xl:pl-72">
+        <main>
+            <header class="flex items-center justify-between border-b border-white/5 px-4 py-4 sm:px-6 sm:py-6 lg:px-8">
+                <h1 class="text-base font-semibold leading-7 text-white">
+                    {{ title }}
+                </h1>
+            </header>
+
+            <main>
+                <div class="mx-auto max-w-full sm:px-6 lg:px-8 py-4">
+                    <slot />
+                </div>
+            </main>
+        </main>
+    </div>
+</template>
