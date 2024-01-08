1e62cbe019bbd142009aa5e9af4bec16aee72cb9
egarcia <egarcia@gtssl.com>
2024-01-08 23:48:18 +0100

    12.2 Componente Vue SidebarHelloUser
    	new file:   resources/js/components/ui/sidebars/items/AppSidebarHelloUserItem.vue

---
+++ resources/js/components/ui/sidebars/items/AppSidebarHelloUserItem.vue
@@ -0,0 +1,22 @@
+<script>
+export default {
+    name: 'AppSidebarHelloUserItem',
+}
+</script>
+
+<script setup>
+defineProps({
+    text: {
+        type: String,
+        required: true,
+    },
+})
+</script>
+
+<template>
+    <li class="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
+        <span class="flex-1 h-px bg-gray-300 dark:bg-gray-700" aria-hidden="true"></span>
+        <span class="px-2 text-sm text-gray-500 dark:text-gray-400">{{ text }}</span>
+        <span class="flex-1 h-px bg-gray-300 dark:bg-gray-700" aria-hidden="true"></span>
+    </li>
+</template>
