9baf8bb5fca5403df503602bf3e9348d19da7caf
egarcia <egarcia@gtssl.com>
2024-01-09 00:08:12 +0100

    12.6 Componente Sidebar Vue para iterar los componentes de navegación
    	new file:   resources/js/components/ui/sidebars/AppSidebar.vue

---
+++ resources/js/components/ui/sidebars/AppSidebar.vue
@@ -0,0 +1,52 @@
+<script>
+export default {
+    name: 'AppSidebar',
+}
+</script>
+
+<script setup>
+import {
+    AppSidebarLinkItem,
+    AppSidebarSeparatorItem,
+    AppSidebarHelloUserItem
+} from "@/components/ui/sidebars/items";
+
+defineProps({
+    title: {
+        type: String,
+        required: true,
+    },
+    user: {
+        type: Object,
+        required: true,
+    },
+    sidebarNavItems: {
+        type: Array,
+        required: true,
+    },
+})
+
+const sidebarComponents = {
+    AppSidebarLinkItem,
+    AppSidebarSeparatorItem,
+    AppSidebarHelloUserItem,
+}
+</script>
+
+<template>
+    <aside
+        class="fixed top-0 left-0 z-40 w-64 h-screen transition-transform -translate-x-full sm:translate-x-0"
+        aria-label="Sidebar"
+    >
+        <div class="h-full px-3 py-4 overflow-y-auto bg-gray-50 dark:bg-gray-800">
+            <ul class="space-y-2 font-medium">
+                <component
+                    v-for="(item, index) in sidebarNavItems"
+                    :key="`sidebar-nav-item-${index}`"
+                    :is="sidebarComponents[item.component]"
+                    v-bind="item"
+                />
+            </ul>
+        </div>
+    </aside>
+</template>
