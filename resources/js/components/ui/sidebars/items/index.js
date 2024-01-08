6c333f2b38b6d851fc5fcdf0a7733cddffb861d9
egarcia <egarcia@gtssl.com>
2024-01-08 23:56:53 +0100

    12.5 Importando y exportando componentes de forma ordenada
    	new file:   resources/js/components/ui/sidebars/items/index.js

---
+++ resources/js/components/ui/sidebars/items/index.js
@@ -0,0 +1,5 @@
+import AppSidebarLinkItem from './AppSidebarLinkItem.vue';
+import AppSidebarSeparatorItem from './AppSidebarSeparatorItem.vue';
+import AppSidebarHelloUserItem from './AppSidebarHelloUserItem.vue';
+
+export { AppSidebarLinkItem, AppSidebarSeparatorItem, AppSidebarHelloUserItem };
