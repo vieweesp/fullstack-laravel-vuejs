349475e5a7dd1a73f1cc8fa7f2105eea3dbe73c7
egarcia <egarcia@gtssl.com>
2024-01-08 23:52:33 +0100

    12.3 Componente Vue SidebarLink
    	new file:   resources/js/components/ui/sidebars/items/AppSidebarLinkItem.vue

---
+++ resources/js/components/ui/sidebars/items/AppSidebarLinkItem.vue
@@ -0,0 +1,44 @@
+<script>
+export default {
+    name: 'AppSidebarLinkItem',
+}
+</script>
+
+<script setup>
+import useIconLoader from "@/composables/useIconLoader.js";
+
+defineProps({
+    text: {
+        type: String,
+        required: true,
+    },
+    href: {
+        type: String,
+        required: true,
+    },
+    iconComponent: {
+        type: String,
+        required: true,
+    },
+    current: {
+        type: Boolean,
+        required: true,
+    },
+})
+
+const { getIcon } = useIconLoader()
+</script>
+
+<template>
+    <li
+        class="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group"
+    >
+        <a
+            :href="href"
+            :class="[current ? 'bg-gray-800 text-white' : 'text-gray-400 hover:text-white hover:bg-gray-800', 'group flex gap-x-3 rounded-md p-2 text-sm leading-6 font-semibold']"
+        >
+            <component :is="getIcon(iconComponent)" class="h-6 w-6 shrink-0" aria-hidden="true" />
+            {{ text }}
+        </a>
+    </li>
+</template>
