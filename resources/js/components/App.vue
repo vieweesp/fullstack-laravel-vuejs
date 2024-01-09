8280337b67726b685289947abe1daba6525dafd7
egarcia <egarcia@gtssl.com>
2024-01-09 01:03:01 +0100

    13.2 Publicar BackofficeLayout de Vue para que se pueda utilizar en
    	modified:   resources/js/components/App.vue

--- resources/js/components/App.vue
+++ resources/js/components/App.vue
@@ -1,8 +1,13 @@
 <script>
+import {defineAsyncComponent} from "vue";
+
+const BackOfficeLayout = defineAsyncComponent(() => import('@/layouts/BackOfficeLayout.vue'));
+
 export default {
     name: 'App',
     components: {
 
+        BackOfficeLayout,
     },
 
     setup() {
